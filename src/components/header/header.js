cd212e13ac45e535c779504061a5b64bdb633696
Nima <www.nimamahdi1385@gmail.com>
2025-04-06 15:05:34 +0330

    Finishing Search Input

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -2,6 +2,8 @@ import React from "react";
 import "../header/header.css";
 import Logo from "../../assets/images/Logo.jpg";
 
+import SearchIcon from "@mui/icons-material/Search";
+
 const Header = () => {
   return (
     <>
@@ -9,15 +11,17 @@ const Header = () => {
         <div className="container-fluid">
           <div className="row">
             <div className="col-sm-2">
-              <img src={Logo} />
+              <img src={Logo} alt="Logo" />
             </div>
 
             {/* Header Search Start */}
             <div className="col-sm-5">
               <div className="headerSearch d-flex align-items-center">
-                <div className="selectDrop">همه دسته بندی ها</div>
+                <div className="selectDrop cursor">همه دسته بندی ها</div>
+
                 <div className="search">
-                  <input type="text" />
+                  <input type="text" placeholder="جستجو کالا" />
+                  <SearchIcon className="searchIcon cursor" />
                 </div>
               </div>
             </div>
