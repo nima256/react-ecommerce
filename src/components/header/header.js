908cc2cd3a13177488d3847139821e44cf823135
Nima <www.nimamahdi1385@gmail.com>
2025-04-07 19:03:25 +0330

    Adding Cities Select Box With Api

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -3,8 +3,46 @@ import Logo from "../../assets/images/Logo.jpg";
 import Select from "../selectDrop/select";
 
 import SearchIcon from "@mui/icons-material/Search";
+import LocationOnOutlinedIcon from "@mui/icons-material/LocationOnOutlined";
+
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const Header = () => {
+  const [categories, setCategories] = useState([
+    "پیراهن",
+    "تی شرت",
+    "شلوار",
+    "کلاه",
+    "کفش",
+    "کت",
+    "عینک",
+    "کاپشن",
+    "جوراب",
+    "شلوارک",
+    "تاب",
+  ]);
+
+  const countryList = [];
+
+  useEffect(() => {
+    getCountry("https://www.iran-locations-api.ir/api/v1/fa/states");
+  }, []);
+
+  const getCountry = async (url) => {
+    try {
+      await axios.get(url).then((res) => {
+        if (res !== null) {
+          res.data.map((item) => {
+            countryList.push(item.name);
+          });
+        }
+      });
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
   return (
     <>
       <header>
@@ -15,16 +53,31 @@ const Header = () => {
             </div>
 
             {/* Header Search Start */}
-            <div className="col-sm-5">
+            <div className="col-sm-5 d-flex align-items-center">
               <div className="headerSearch d-flex align-items-center">
-                <Select />
+                <Select
+                  data={categories}
+                  placeholder={"همه دسته بندی ها"}
+                  icon={false}
+                />
+
                 <div className="search">
-                  <input type="text" placeholder="جستجو کالا" />
+                  <input type="text" placeholder="جستجو کالا..." />
                   <SearchIcon className="searchIcon cursor" />
                 </div>
               </div>
             </div>
             {/* Header Search End */}
+
+            <div className="col-sm-5 d-flex align-items-center">
+              <div className="countryWrapper">
+                <Select
+                  data={countryList}
+                  placeholder={"مکان شما"}
+                  icon={<LocationOnOutlinedIcon style={{ opacity: "0.5" }} />}
+                />
+              </div>
+            </div>
           </div>
         </div>
       </header>
--- src/components/selectDrop/select.js
+++ src/components/selectDrop/select.js
@@ -2,110 +2,60 @@ import { useState } from "react";
 import "../selectDrop/select.css";
 
 import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+import ClickAwayListener from "@mui/material/ClickAwayListener";
 
-const Select = () => {
+const Select = ({ data, placeholder, icon }) => {
   const [isOpenSelect, setIsOpenSelect] = useState(false);
   const [selectedIndex, setSelectedIndex] = useState(0);
-  const [selectedItem, setSelectedItem] = useState("همه دسته بندی ها");
+  const [selectedItem, setSelectedItem] = useState(placeholder);
 
   const openSelect = () => {
     setIsOpenSelect(!isOpenSelect);
   };
 
-  const closeSelect = (index, e) => {
+  const closeSelect = (index, item) => {
     setSelectedIndex(index);
     setIsOpenSelect(false);
-    setSelectedItem(e.target.textContent);
+    setSelectedItem(item);
   };
 
   return (
-    <div className="selectDrop cursor">
-      <span className="openSelect" onClick={openSelect}>
-        {selectedItem} <ArrowDropDownIcon className="dropDownArrow" />
-      </span>
+    <ClickAwayListener onClickAway={() => setIsOpenSelect(false)}>
+      <div className="selectDropWrapper cursor position-relative">
+        {icon}
+        <span className="selectDropp" onClick={openSelect}>
+          {selectedItem} <ArrowDropDownIcon className="dropDownArrow" />
+        </span>
 
-      {isOpenSelect === true && (
-        <div className="selectDropp">
-          <div className="searchField">
-            <input type="text" />
+        {isOpenSelect === true && (
+          <div className="selectDrop">
+            <div className="searchField">
+              <input type="text" placeholder="جستجو دسته بندی..." />
+            </div>
+            <ul className="searchResults">
+              <li
+                key={0}
+                onClick={() => closeSelect(0, placeholder)}
+                className={`${selectedIndex === 0 ? "active" : ""}`}
+              >
+                {placeholder}
+              </li>
+              {data.map((item, index) => {
+                return (
+                  <li
+                    key={index + 1}
+                    onClick={() => closeSelect(index + 1, item)}
+                    className={`${selectedIndex === index + 1 ? "active" : ""}`}
+                  >
+                    {item}
+                  </li>
+                );
+              })}
+            </ul>
           </div>
-          <ul className="searchResults">
-            <li
-              onClick={(e) => closeSelect(0, e)}
-              className={`${selectedIndex === 0 ? "active" : ""}`}
-            >
-              همه دسته بندی ها
-            </li>
-            <li
-              onClick={(e) => closeSelect(1, e)}
-              className={`${selectedIndex === 1 ? "active" : ""}`}
-            >
-              پیراهن
-            </li>
-            <li
-              onClick={(e) => closeSelect(2, e)}
-              className={`${selectedIndex === 2 ? "active" : ""}`}
-            >
-              تی شرت
-            </li>
-            <li
-              onClick={(e) => closeSelect(3, e)}
-              className={`${selectedIndex === 3 ? "active" : ""}`}
-            >
-              شلوار
-            </li>
-            <li
-              onClick={(e) => closeSelect(4, e)}
-              className={`${selectedIndex === 4 ? "active" : ""}`}
-            >
-              کلاه
-            </li>
-            <li
-              onClick={(e) => closeSelect(5, e)}
-              className={`${selectedIndex === 5 ? "active" : ""}`}
-            >
-              کفش
-            </li>
-            <li
-              onClick={(e) => closeSelect(6, e)}
-              className={`${selectedIndex === 6 ? "active" : ""}`}
-            >
-              کت
-            </li>
-            <li
-              onClick={(e) => closeSelect(7, e)}
-              className={`${selectedIndex === 7 ? "active" : ""}`}
-            >
-              عینک
-            </li>
-            <li
-              onClick={(e) => closeSelect(8, e)}
-              className={`${selectedIndex === 8 ? "active" : ""}`}
-            >
-              کاپشن
-            </li>
-            <li
-              onClick={(e) => closeSelect(9, e)}
-              className={`${selectedIndex === 9 ? "active" : ""}`}
-            >
-              جوراب
-            </li>
-            <li
-              onClick={(e) => closeSelect(10, e)}
-              className={`${selectedIndex === 10 ? "active" : ""}`}
-            >
-              شلوارک
-            </li>
-            <li
-              onClick={(e) => closeSelect(11, e)}
-              className={`${selectedIndex === 11 ? "active" : ""}`}
-            >
-              تاب
-            </li>
-          </ul>
-        </div>
-      )}
-    </div>
+        )}
+      </div>
+    </ClickAwayListener>
   );
 };
 
