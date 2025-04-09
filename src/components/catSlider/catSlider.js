c74001b6bbb7cc8ce876509237086e793d4c7ef7
Nima <www.nimamahdi1385@gmail.com>
2025-04-09 21:41:02 +0330

    Adding Category Meny

---
+++ src/components/catSlider/catSlider.js
@@ -0,0 +1,226 @@
+import "./catSlider.css";
+import Slider from "react-slick";
+
+const CatSlider = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 10,
+    slidesToScroll: 1,
+    arrows: false,
+    rtl: true,
+  };
+
+  return (
+    <>
+      <div className="catSliderSection">
+        <div className="container-fluid cat-container">
+          <h2 className="hd">دسته بندی ها پر طرفدار</h2>
+          <Slider {...settings} className="cat_slider_main">
+            <div className="item">
+              <div className="info">
+                <img src="" alt="" />
+                <h5>شلوار</h5>
+                <p>28 عدد</p>
+              </div>
+            </div>
+            <div className="item">
+              <div className="info">
+                <img src="" alt="" />
+                <h5>شلوار</h5>
+                <p>28 عدد</p>
+              </div>
+            </div>
+            <div className="item">
+              <div className="info">
+                <img src="" alt="" />
+                <h5>شلوار</h5>
+                <p>28 عدد</p>
+              </div>
+            </div>
+            <div className="item">
+              <div className="info">
+                <img src="" alt="" />
+                <h5>شلوار</h5>
+                <p>28 عدد</p>
+              </div>
+            </div>
+            <div className="item">
+              <div className="info">
+                <img src="" alt="" />
+                <h5>شلوار</h5>
+                <p>28 عدد</p>
+              </div>
+            </div>
+            <div className="item">
+              <div className="info">
+                <img src="" alt="" />
+                <h5>شلوار</h5>
+                <p>28 عدد</p>
+              </div>
+            </div>
+            <div className="item">
+              <div className="info">
+                <img src="" alt="" />
+                <h5>شلوار</h5>
+                <p>28 عدد</p>
+              </div>
+            </div>
+            <div className="item">
+              <div className="info">
+                <img src="" alt="" />
+                <h5>شلوار</h5>
+                <p>28 عدد</p>
+              </div>
+            </div>
+            <div className="item">
+              <div className="info">
+                <img src="" alt="" />
+                <h5>شلوار</h5>
+                <p>28 عدد</p>
+              </div>
+            </div>
+            <div className="item">
+              <div className="info">
+                <img src="" alt="" />
+                <h5>شلوار</h5>
+                <p>28 عدد</p>
+              </div>
+            </div>
+            <div className="item">
+              <div className="info">
+                <img src="" alt="" />
+                <h5>شلوار</h5>
+                <p>28 عدد</p>
+              </div>
+            </div>
+            <div className="item">
+              <div className="info">
+                <img src="" alt="" />
+                <h5>شلوار</h5>
+                <p>28 عدد</p>
+              </div>
+            </div>
+            <div className="item">
+              <div className="info">
+                <img src="" alt="" />
+                <h5>شلوار</h5>
+                <p>28 عدد</p>
+              </div>
+            </div>
+            <div className="item">
+              <div className="info">
+                <img src="" alt="" />
+                <h5>شلوار</h5>
+                <p>28 عدد</p>
+              </div>
+            </div>
+            <div className="item">
+              <div className="info">
+                <img src="" alt="" />
+                <h5>شلوار</h5>
+                <p>28 عدد</p>
+              </div>
+            </div>
+            <div className="item">
+              <div className="info">
+                <img src="" alt="" />
+                <h5>شلوار</h5>
+                <p>28 عدد</p>
+              </div>
+            </div>
+            <div className="item">
+              <div className="info">
+                <img src="" alt="" />
+                <h5>شلوار</h5>
+                <p>28 عدد</p>
+              </div>
+            </div>
+            <div className="item">
+              <div className="info">
+                <img src="" alt="" />
+                <h5>شلوار</h5>
+                <p>28 عدد</p>
+              </div>
+            </div>
+            <div className="item">
+              <div className="info">
+                <img src="" alt="" />
+                <h5>شلوار</h5>
+                <p>28 عدد</p>
+              </div>
+            </div>
+            <div className="item">
+              <div className="info">
+                <img src="" alt="" />
+                <h5>شلوار</h5>
+                <p>28 عدد</p>
+              </div>
+            </div>
+            <div className="item">
+              <div className="info">
+                <img src="" alt="" />
+                <h5>شلوار</h5>
+                <p>28 عدد</p>
+              </div>
+            </div>
+            <div className="item">
+              <div className="info">
+                <img src="" alt="" />
+                <h5>شلوار</h5>
+                <p>28 عدد</p>
+              </div>
+            </div>
+            <div className="item">
+              <div className="info">
+                <img src="" alt="" />
+                <h5>شلوار</h5>
+                <p>28 عدد</p>
+              </div>
+            </div>
+            <div className="item">
+              <div className="info">
+                <img src="" alt="" />
+                <h5>شلوار</h5>
+                <p>28 عدد</p>
+              </div>
+            </div>
+            <div className="item">
+              <div className="info">
+                <img src="" alt="" />
+                <h5>شلوار</h5>
+                <p>28 عدد</p>
+              </div>
+            </div>
+            <div className="item">
+              <div className="info">
+                <img src="" alt="" />
+                <h5>شلوار</h5>
+                <p>28 عدد</p>
+              </div>
+            </div>
+            <div className="item">
+              <div className="info">
+                <img src="" alt="" />
+                <h5>شلوار</h5>
+                <p>28 عدد</p>
+              </div>
+            </div>
+          </Slider>
+        </div>
+      </div>
+
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+    </>
+  );
+};
+
+export default CatSlider;
--- src/pages/Home/home.js
+++ src/pages/Home/home.js
@@ -1,9 +1,11 @@
+import CatSlider from "../../components/catSlider/catSlider";
 import HomeSlider from "./slider/slider";
 
 const Home = () => {
   return (
     <>
       <HomeSlider />
+      <CatSlider />
     </>
   );
 };
