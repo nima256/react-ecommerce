241d55b31f875096f8e2b1021a64385e7325320c
Nima <www.nimamahdi1385@gmail.com>
2025-04-12 21:59:06 +0330

    Finishing Popular Categories

--- src/components/catSlider/catSlider.js
+++ src/components/catSlider/catSlider.js
@@ -1,14 +1,39 @@
 import "./catSlider.css";
+import categoryImg from "../../assets/images/category-image.png";
 import Slider from "react-slick";
+import { useState } from "react";
 
 const CatSlider = () => {
+  const [itemBg, setItemBg] = useState([
+    "#fffceb",
+    "#ecffec",
+    "#feefea",
+    "#fff3eb",
+    "#fff3ff",
+    "#f2fce4",
+    "#feefea",
+    "#fffceb",
+    "#feefea",
+    "#ecffec",
+    "#fffceb",
+    "#ecffec",
+    "#feefea",
+    "#fff3eb",
+    "#fff3ff",
+    "#f2fce4",
+    "#feefea",
+    "#fffceb",
+    "#feefea",
+    "#ecffec",
+  ]);
+
   const settings = {
     dots: true,
     infinite: true,
     speed: 500,
     slidesToShow: 10,
     slidesToScroll: 1,
-    arrows: false,
+    arrows: true,
     rtl: true,
   };
 
@@ -18,195 +43,95 @@ const CatSlider = () => {
         <div className="container-fluid cat-container">
           <h2 className="hd">دسته بندی ها پر طرفدار</h2>
           <Slider {...settings} className="cat_slider_main">
-            <div className="item">
+            {itemBg.length !== 0 &&
+              itemBg.map((item, index) => {
+                return (
+                  <div className="item">
+                    <div className="info" style={{ background: item }}>
+                      <img src={categoryImg} alt="" className="categoryImg" />
+                      <h5>پیراهن</h5>
+                      <p dir="rtl">۲۸ عدد</p>
+                    </div>
+                  </div>
+                );
+              })}
+            {/* <div className="item">
               <div className="info">
-                <img src="" alt="" />
-                <h5>شلوار</h5>
-                <p>28 عدد</p>
+                <img src={categoryImg} alt="" className="categoryImg" />
+                <h5>پیراهن</h5>
+                <p dir="rtl">۲۸ عدد</p>
               </div>
             </div>
             <div className="item">
               <div className="info">
-                <img src="" alt="" />
-                <h5>شلوار</h5>
-                <p>28 عدد</p>
+                <img src={categoryImg} alt="" className="categoryImg" />
+                <h5>تی شرت</h5>
+                <p dir="rtl">۲۸ عدد</p>
               </div>
             </div>
             <div className="item">
               <div className="info">
-                <img src="" alt="" />
+                <img src={categoryImg} alt="" className="categoryImg" />
                 <h5>شلوار</h5>
-                <p>28 عدد</p>
+                <p dir="rtl">۲۸ عدد</p>{" "}
               </div>
             </div>
             <div className="item">
               <div className="info">
-                <img src="" alt="" />
-                <h5>شلوار</h5>
-                <p>28 عدد</p>
+                <img src={categoryImg} alt="" className="categoryImg" />
+                <h5>کفش</h5>
+                <p dir="rtl">۲۸ عدد</p>{" "}
               </div>
             </div>
             <div className="item">
               <div className="info">
-                <img src="" alt="" />
-                <h5>شلوار</h5>
-                <p>28 عدد</p>
+                <img src={categoryImg} alt="" className="categoryImg" />
+                <h5>کاپشن</h5>
+                <p dir="rtl">۲۸ عدد</p>{" "}
               </div>
             </div>
             <div className="item">
               <div className="info">
-                <img src="" alt="" />
-                <h5>شلوار</h5>
-                <p>28 عدد</p>
+                <img src={categoryImg} alt="" className="categoryImg" />
+                <h5>شلوارک</h5>
+                <p dir="rtl">۲۸ عدد</p>{" "}
               </div>
             </div>
             <div className="item">
               <div className="info">
-                <img src="" alt="" />
-                <h5>شلوار</h5>
-                <p>28 عدد</p>
+                <img src={categoryImg} alt="" className="categoryImg" />
+                <h5>تاب</h5>
+                <p dir="rtl">۲۸ عدد</p>{" "}
               </div>
             </div>
             <div className="item">
               <div className="info">
-                <img src="" alt="" />
-                <h5>شلوار</h5>
-                <p>28 عدد</p>
+                <img src={categoryImg} alt="" className="categoryImg" />
+                <h5>کفش</h5>
+                <p dir="rtl">۲۸ عدد</p>{" "}
               </div>
             </div>
             <div className="item">
               <div className="info">
-                <img src="" alt="" />
-                <h5>شلوار</h5>
-                <p>28 عدد</p>
+                <img src={categoryImg} alt="" className="categoryImg" />
+                <h5>کاپشن</h5>
+                <p dir="rtl">۲۸ عدد</p>{" "}
               </div>
             </div>
             <div className="item">
               <div className="info">
-                <img src="" alt="" />
-                <h5>شلوار</h5>
-                <p>28 عدد</p>
+                <img src={categoryImg} alt="" className="categoryImg" />
+                <h5>شلوارک</h5>
+                <p dir="rtl">۲۸ عدد</p>{" "}
               </div>
             </div>
             <div className="item">
               <div className="info">
-                <img src="" alt="" />
-                <h5>شلوار</h5>
-                <p>28 عدد</p>
+                <img src={categoryImg} alt="" className="categoryImg" />
+                <h5>تاب</h5>
+                <p dir="rtl">۲۸ عدد</p>{" "}
               </div>
-            </div>
-            <div className="item">
-              <div className="info">
-                <img src="" alt="" />
-                <h5>شلوار</h5>
-                <p>28 عدد</p>
-              </div>
-            </div>
-            <div className="item">
-              <div className="info">
-                <img src="" alt="" />
-                <h5>شلوار</h5>
-                <p>28 عدد</p>
-              </div>
-            </div>
-            <div className="item">
-              <div className="info">
-                <img src="" alt="" />
-                <h5>شلوار</h5>
-                <p>28 عدد</p>
-              </div>
-            </div>
-            <div className="item">
-              <div className="info">
-                <img src="" alt="" />
-                <h5>شلوار</h5>
-                <p>28 عدد</p>
-              </div>
-            </div>
-            <div className="item">
-              <div className="info">
-                <img src="" alt="" />
-                <h5>شلوار</h5>
-                <p>28 عدد</p>
-              </div>
-            </div>
-            <div className="item">
-              <div className="info">
-                <img src="" alt="" />
-                <h5>شلوار</h5>
-                <p>28 عدد</p>
-              </div>
-            </div>
-            <div className="item">
-              <div className="info">
-                <img src="" alt="" />
-                <h5>شلوار</h5>
-                <p>28 عدد</p>
-              </div>
-            </div>
-            <div className="item">
-              <div className="info">
-                <img src="" alt="" />
-                <h5>شلوار</h5>
-                <p>28 عدد</p>
-              </div>
-            </div>
-            <div className="item">
-              <div className="info">
-                <img src="" alt="" />
-                <h5>شلوار</h5>
-                <p>28 عدد</p>
-              </div>
-            </div>
-            <div className="item">
-              <div className="info">
-                <img src="" alt="" />
-                <h5>شلوار</h5>
-                <p>28 عدد</p>
-              </div>
-            </div>
-            <div className="item">
-              <div className="info">
-                <img src="" alt="" />
-                <h5>شلوار</h5>
-                <p>28 عدد</p>
-              </div>
-            </div>
-            <div className="item">
-              <div className="info">
-                <img src="" alt="" />
-                <h5>شلوار</h5>
-                <p>28 عدد</p>
-              </div>
-            </div>
-            <div className="item">
-              <div className="info">
-                <img src="" alt="" />
-                <h5>شلوار</h5>
-                <p>28 عدد</p>
-              </div>
-            </div>
-            <div className="item">
-              <div className="info">
-                <img src="" alt="" />
-                <h5>شلوار</h5>
-                <p>28 عدد</p>
-              </div>
-            </div>
-            <div className="item">
-              <div className="info">
-                <img src="" alt="" />
-                <h5>شلوار</h5>
-                <p>28 عدد</p>
-              </div>
-            </div>
-            <div className="item">
-              <div className="info">
-                <img src="" alt="" />
-                <h5>شلوار</h5>
-                <p>28 عدد</p>
-              </div>
-            </div>
+            </div> */}
           </Slider>
         </div>
       </div>
