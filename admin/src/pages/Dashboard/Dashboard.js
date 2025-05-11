81ae06371573d89d6a71ce26fdf31f7d5f0a8a39
Nima <www.nimamahdi1385@gmail.com>
2025-05-11 14:42:46 +0330

    Complete Best Selling Products

--- admin/src/pages/Dashboard/Dashboard.js
+++ admin/src/pages/Dashboard/Dashboard.js
@@ -18,6 +18,11 @@ import { CacheProvider } from "@emotion/react";
 import createCache from "@emotion/cache";
 import FormControl from "@mui/material/FormControl";
 import Select from "@mui/material/Select";
+import { FaEye } from "react-icons/fa";
+import { FaPencilAlt } from "react-icons/fa";
+import { MdDelete } from "react-icons/md";
+import pImg from "../../assets/images/category-image.png";
+import Pagination from "@mui/material/Pagination";
 
 const theme = createTheme({ direction: "rtl" });
 
@@ -184,6 +189,462 @@ function Dashboard() {
                 </ThemeProvider>
               </CacheProvider>
             </div>
+          </div>
+
+          <div className="table-responsive mt-3">
+            <table className="table table-bordered v-align">
+              <thead>
+                <tr className="thead-styles">
+                  <th>عدد</th>
+                  <th>محصول</th>
+                  <th>دسته بندی</th>
+                  <th>برند</th>
+                  <th>قیمت</th>
+                  <th>موجودی</th>
+                  <th>امتیاز</th>
+                  <th>تعداد سفارش</th>
+                  <th>فروش</th>
+                  <th>عملیات ها</th>
+                </tr>
+              </thead>
+
+              <tbody>
+                <tr>
+                  <td>#۱</td>
+                  <td style={{ width: "220px" }}>
+                    <div className="d-flex align-items-center productBox">
+                      <div className="imgWrapper">
+                        <div className="img">
+                          <img src={pImg} alt="" className="w-100" />
+                        </div>
+                      </div>
+                      <div className="info">
+                        <h6>لپ تاپ ایسوس مدل سلام خدافز</h6>
+                        <p>
+                          تی سیتس تی شمسیب تمنشسیب تشسیتمن شسیت تمن شسیتمن منت
+                          شسیتنم منستش منتس
+                        </p>
+                      </div>
+                    </div>
+                  </td>
+                  <td>لپ تاپ</td>
+                  <td>ایسوس</td>
+                  <td>
+                    <del className="old">۳۲,۰۰۰,۰۰۰</del>
+                    <span className="new text-danger">۳۲,۰۰۰,۰۰۰</span>
+                  </td>
+                  <td>۱۲</td>
+                  <td>(۱۶) ۴.۹</td>
+                  <td>۱۲</td>
+                  <td>۶۴,۰۰۰,۰۰۰</td>
+                  <td>
+                    <div className="actions d-flex align-items-center justify-content-evenly">
+                      <Button className="secondary" color="secondary">
+                        <FaEye />
+                      </Button>
+                      <Button className="success" color="success">
+                        <FaPencilAlt />
+                      </Button>
+                      <Button className="error">
+                        <MdDelete color="error" />
+                      </Button>
+                    </div>
+                  </td>
+                </tr>
+                <tr>
+                  <td>#۱</td>
+                  <td style={{ width: "220px" }}>
+                    <div className="d-flex align-items-center productBox">
+                      <div className="imgWrapper">
+                        <div className="img">
+                          <img src={pImg} alt="" className="w-100" />
+                        </div>
+                      </div>
+                      <div className="info">
+                        <h6>لپ تاپ ایسوس مدل سلام خدافز</h6>
+                        <p>
+                          تی سیتس تی شمسیب تمنشسیب تشسیتمن شسیت تمن شسیتمن منت
+                          شسیتنم منستش منتس
+                        </p>
+                      </div>
+                    </div>
+                  </td>
+                  <td>لپ تاپ</td>
+                  <td>ایسوس</td>
+                  <td>
+                    <del className="old">۳۲,۰۰۰,۰۰۰</del>
+                    <span className="new text-danger">۳۲,۰۰۰,۰۰۰</span>
+                  </td>
+                  <td>۱۲</td>
+                  <td>(۱۶) ۴.۹</td>
+                  <td>۱۲</td>
+                  <td>۶۴,۰۰۰,۰۰۰</td>
+                  <td>
+                    <div className="actions d-flex align-items-center justify-content-evenly">
+                      <Button className="secondary" color="secondary">
+                        <FaEye />
+                      </Button>
+                      <Button className="success" color="success">
+                        <FaPencilAlt />
+                      </Button>
+                      <Button className="error">
+                        <MdDelete color="error" />
+                      </Button>
+                    </div>
+                  </td>
+                </tr>
+                <tr>
+                  <td>#۱</td>
+                  <td style={{ width: "220px" }}>
+                    <div className="d-flex align-items-center productBox">
+                      <div className="imgWrapper">
+                        <div className="img">
+                          <img src={pImg} alt="" className="w-100" />
+                        </div>
+                      </div>
+                      <div className="info">
+                        <h6>لپ تاپ ایسوس مدل سلام خدافز</h6>
+                        <p>
+                          تی سیتس تی شمسیب تمنشسیب تشسیتمن شسیت تمن شسیتمن منت
+                          شسیتنم منستش منتس
+                        </p>
+                      </div>
+                    </div>
+                  </td>
+                  <td>لپ تاپ</td>
+                  <td>ایسوس</td>
+                  <td>
+                    <del className="old">۳۲,۰۰۰,۰۰۰</del>
+                    <span className="new text-danger">۳۲,۰۰۰,۰۰۰</span>
+                  </td>
+                  <td>۱۲</td>
+                  <td>(۱۶) ۴.۹</td>
+                  <td>۱۲</td>
+                  <td>۶۴,۰۰۰,۰۰۰</td>
+                  <td>
+                    <div className="actions d-flex align-items-center justify-content-evenly">
+                      <Button className="secondary" color="secondary">
+                        <FaEye />
+                      </Button>
+                      <Button className="success" color="success">
+                        <FaPencilAlt />
+                      </Button>
+                      <Button className="error">
+                        <MdDelete color="error" />
+                      </Button>
+                    </div>
+                  </td>
+                </tr>
+                <tr>
+                  <td>#۱</td>
+                  <td style={{ width: "220px" }}>
+                    <div className="d-flex align-items-center productBox">
+                      <div className="imgWrapper">
+                        <div className="img">
+                          <img src={pImg} alt="" className="w-100" />
+                        </div>
+                      </div>
+                      <div className="info">
+                        <h6>لپ تاپ ایسوس مدل سلام خدافز</h6>
+                        <p>
+                          تی سیتس تی شمسیب تمنشسیب تشسیتمن شسیت تمن شسیتمن منت
+                          شسیتنم منستش منتس
+                        </p>
+                      </div>
+                    </div>
+                  </td>
+                  <td>لپ تاپ</td>
+                  <td>ایسوس</td>
+                  <td>
+                    <del className="old">۳۲,۰۰۰,۰۰۰</del>
+                    <span className="new text-danger">۳۲,۰۰۰,۰۰۰</span>
+                  </td>
+                  <td>۱۲</td>
+                  <td>(۱۶) ۴.۹</td>
+                  <td>۱۲</td>
+                  <td>۶۴,۰۰۰,۰۰۰</td>
+                  <td>
+                    <div className="actions d-flex align-items-center justify-content-evenly">
+                      <Button className="secondary" color="secondary">
+                        <FaEye />
+                      </Button>
+                      <Button className="success" color="success">
+                        <FaPencilAlt />
+                      </Button>
+                      <Button className="error">
+                        <MdDelete color="error" />
+                      </Button>
+                    </div>
+                  </td>
+                </tr>
+                <tr>
+                  <td>#۱</td>
+                  <td style={{ width: "220px" }}>
+                    <div className="d-flex align-items-center productBox">
+                      <div className="imgWrapper">
+                        <div className="img">
+                          <img src={pImg} alt="" className="w-100" />
+                        </div>
+                      </div>
+                      <div className="info">
+                        <h6>لپ تاپ ایسوس مدل سلام خدافز</h6>
+                        <p>
+                          تی سیتس تی شمسیب تمنشسیب تشسیتمن شسیت تمن شسیتمن منت
+                          شسیتنم منستش منتس
+                        </p>
+                      </div>
+                    </div>
+                  </td>
+                  <td>لپ تاپ</td>
+                  <td>ایسوس</td>
+                  <td>
+                    <del className="old">۳۲,۰۰۰,۰۰۰</del>
+                    <span className="new text-danger">۳۲,۰۰۰,۰۰۰</span>
+                  </td>
+                  <td>۱۲</td>
+                  <td>(۱۶) ۴.۹</td>
+                  <td>۱۲</td>
+                  <td>۶۴,۰۰۰,۰۰۰</td>
+                  <td>
+                    <div className="actions d-flex align-items-center justify-content-evenly">
+                      <Button className="secondary" color="secondary">
+                        <FaEye />
+                      </Button>
+                      <Button className="success" color="success">
+                        <FaPencilAlt />
+                      </Button>
+                      <Button className="error">
+                        <MdDelete color="error" />
+                      </Button>
+                    </div>
+                  </td>
+                </tr>
+                <tr>
+                  <td>#۱</td>
+                  <td style={{ width: "220px" }}>
+                    <div className="d-flex align-items-center productBox">
+                      <div className="imgWrapper">
+                        <div className="img">
+                          <img src={pImg} alt="" className="w-100" />
+                        </div>
+                      </div>
+                      <div className="info">
+                        <h6>لپ تاپ ایسوس مدل سلام خدافز</h6>
+                        <p>
+                          تی سیتس تی شمسیب تمنشسیب تشسیتمن شسیت تمن شسیتمن منت
+                          شسیتنم منستش منتس
+                        </p>
+                      </div>
+                    </div>
+                  </td>
+                  <td>لپ تاپ</td>
+                  <td>ایسوس</td>
+                  <td>
+                    <del className="old">۳۲,۰۰۰,۰۰۰</del>
+                    <span className="new text-danger">۳۲,۰۰۰,۰۰۰</span>
+                  </td>
+                  <td>۱۲</td>
+                  <td>(۱۶) ۴.۹</td>
+                  <td>۱۲</td>
+                  <td>۶۴,۰۰۰,۰۰۰</td>
+                  <td>
+                    <div className="actions d-flex align-items-center justify-content-evenly">
+                      <Button className="secondary" color="secondary">
+                        <FaEye />
+                      </Button>
+                      <Button className="success" color="success">
+                        <FaPencilAlt />
+                      </Button>
+                      <Button className="error">
+                        <MdDelete color="error" />
+                      </Button>
+                    </div>
+                  </td>
+                </tr>
+                <tr>
+                  <td>#۱</td>
+                  <td style={{ width: "220px" }}>
+                    <div className="d-flex align-items-center productBox">
+                      <div className="imgWrapper">
+                        <div className="img">
+                          <img src={pImg} alt="" className="w-100" />
+                        </div>
+                      </div>
+                      <div className="info">
+                        <h6>لپ تاپ ایسوس مدل سلام خدافز</h6>
+                        <p>
+                          تی سیتس تی شمسیب تمنشسیب تشسیتمن شسیت تمن شسیتمن منت
+                          شسیتنم منستش منتس
+                        </p>
+                      </div>
+                    </div>
+                  </td>
+                  <td>لپ تاپ</td>
+                  <td>ایسوس</td>
+                  <td>
+                    <del className="old">۳۲,۰۰۰,۰۰۰</del>
+                    <span className="new text-danger">۳۲,۰۰۰,۰۰۰</span>
+                  </td>
+                  <td>۱۲</td>
+                  <td>(۱۶) ۴.۹</td>
+                  <td>۱۲</td>
+                  <td>۶۴,۰۰۰,۰۰۰</td>
+                  <td>
+                    <div className="actions d-flex align-items-center justify-content-evenly">
+                      <Button className="secondary" color="secondary">
+                        <FaEye />
+                      </Button>
+                      <Button className="success" color="success">
+                        <FaPencilAlt />
+                      </Button>
+                      <Button className="error">
+                        <MdDelete color="error" />
+                      </Button>
+                    </div>
+                  </td>
+                </tr>
+                <tr>
+                  <td>#۱</td>
+                  <td style={{ width: "220px" }}>
+                    <div className="d-flex align-items-center productBox">
+                      <div className="imgWrapper">
+                        <div className="img">
+                          <img src={pImg} alt="" className="w-100" />
+                        </div>
+                      </div>
+                      <div className="info">
+                        <h6>لپ تاپ ایسوس مدل سلام خدافز</h6>
+                        <p>
+                          تی سیتس تی شمسیب تمنشسیب تشسیتمن شسیت تمن شسیتمن منت
+                          شسیتنم منستش منتس
+                        </p>
+                      </div>
+                    </div>
+                  </td>
+                  <td>لپ تاپ</td>
+                  <td>ایسوس</td>
+                  <td>
+                    <del className="old">۳۲,۰۰۰,۰۰۰</del>
+                    <span className="new text-danger">۳۲,۰۰۰,۰۰۰</span>
+                  </td>
+                  <td>۱۲</td>
+                  <td>(۱۶) ۴.۹</td>
+                  <td>۱۲</td>
+                  <td>۶۴,۰۰۰,۰۰۰</td>
+                  <td>
+                    <div className="actions d-flex align-items-center justify-content-evenly">
+                      <Button className="secondary" color="secondary">
+                        <FaEye />
+                      </Button>
+                      <Button className="success" color="success">
+                        <FaPencilAlt />
+                      </Button>
+                      <Button className="error">
+                        <MdDelete color="error" />
+                      </Button>
+                    </div>
+                  </td>
+                </tr>
+                <tr>
+                  <td>#۱</td>
+                  <td style={{ width: "220px" }}>
+                    <div className="d-flex align-items-center productBox">
+                      <div className="imgWrapper">
+                        <div className="img">
+                          <img src={pImg} alt="" className="w-100" />
+                        </div>
+                      </div>
+                      <div className="info">
+                        <h6>لپ تاپ ایسوس مدل سلام خدافز</h6>
+                        <p>
+                          تی سیتس تی شمسیب تمنشسیب تشسیتمن شسیت تمن شسیتمن منت
+                          شسیتنم منستش منتس
+                        </p>
+                      </div>
+                    </div>
+                  </td>
+                  <td>لپ تاپ</td>
+                  <td>ایسوس</td>
+                  <td>
+                    <del className="old">۳۲,۰۰۰,۰۰۰</del>
+                    <span className="new text-danger">۳۲,۰۰۰,۰۰۰</span>
+                  </td>
+                  <td>۱۲</td>
+                  <td>(۱۶) ۴.۹</td>
+                  <td>۱۲</td>
+                  <td>۶۴,۰۰۰,۰۰۰</td>
+                  <td>
+                    <div className="actions d-flex align-items-center justify-content-evenly">
+                      <Button className="secondary" color="secondary">
+                        <FaEye />
+                      </Button>
+                      <Button className="success" color="success">
+                        <FaPencilAlt />
+                      </Button>
+                      <Button className="error">
+                        <MdDelete color="error" />
+                      </Button>
+                    </div>
+                  </td>
+                </tr>
+                <tr>
+                  <td>#۱</td>
+                  <td style={{ width: "220px" }}>
+                    <div className="d-flex align-items-center productBox">
+                      <div className="imgWrapper">
+                        <div className="img">
+                          <img src={pImg} alt="" className="w-100" />
+                        </div>
+                      </div>
+                      <div className="info">
+                        <h6>لپ تاپ ایسوس مدل سلام خدافز</h6>
+                        <p>
+                          تی سیتس تی شمسیب تمنشسیب تشسیتمن شسیت تمن شسیتمن منت
+                          شسیتنم منستش منتس
+                        </p>
+                      </div>
+                    </div>
+                  </td>
+                  <td>لپ تاپ</td>
+                  <td>ایسوس</td>
+                  <td>
+                    <del className="old">۳۲,۰۰۰,۰۰۰</del>
+                    <span className="new text-danger">۳۲,۰۰۰,۰۰۰</span>
+                  </td>
+                  <td>۱۲</td>
+                  <td>(۱۶) ۴.۹</td>
+                  <td>۱۲</td>
+                  <td>۶۴,۰۰۰,۰۰۰</td>
+                  <td>
+                    <div className="actions d-flex align-items-center justify-content-evenly">
+                      <Button className="secondary" color="secondary">
+                        <FaEye />
+                      </Button>
+                      <Button className="success" color="success">
+                        <FaPencilAlt />
+                      </Button>
+                      <Button className="error">
+                        <MdDelete color="error" />
+                      </Button>
+                    </div>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+
+            <CacheProvider value={cacheRtl}>
+              <ThemeProvider theme={theme}>
+                <div className="d-flex tableFooter">
+                  <Pagination
+                    count={10}
+                    color="primary"
+                    className="pagination"
+                    showFirstButton
+                    showLastButton
+                  />
+                </div>
+              </ThemeProvider>
+            </CacheProvider>
           </div>
         </div>
       </div>
