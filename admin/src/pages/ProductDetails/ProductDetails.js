a613696448f45eb4baecfe307008c16c3749f0a1
Nima <www.nimamahdi1385@gmail.com>
2025-05-13 21:36:40 +0330

    Adding Product Details

---
+++ admin/src/pages/ProductDetails/ProductDetails.js
@@ -0,0 +1,13 @@
+import './ProductDetails.css'
+
+function ProductDetails() {
+    return (
+        <>
+            <div className="right-content w-100">
+                
+            </div>
+        </>
+    )
+}
+
+export default ProductDetails
